c4fcdfc7eb5593f449903d4cba731ae9d0784ff5
Kalli Bonin <kalli_bonin@yahoo.com>
2019-01-21 22:15:20 -0500

    Add files via upload

---
+++ Lesson_6_Fractal/Lesson_6_Fractal/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Lesson_6_Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbFractal = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIterations = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFractal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbFractal
+            // 
+            this.pbFractal.Location = new System.Drawing.Point(12, 12);
+            this.pbFractal.Name = "pbFractal";
+            this.pbFractal.Size = new System.Drawing.Size(600, 600);
+            this.pbFractal.TabIndex = 0;
+            this.pbFractal.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Iterations:";
+            // 
+            // txtIterations
+            // 
+            this.txtIterations.Location = new System.Drawing.Point(618, 29);
+            this.txtIterations.Name = "txtIterations";
+            this.txtIterations.Size = new System.Drawing.Size(100, 20);
+            this.txtIterations.TabIndex = 2;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(618, 55);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(100, 23);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 623);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtIterations);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbFractal);
+            this.Name = "Form1";
+            this.Text = "Fractal Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFractal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbFractal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIterations;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ Lesson_6_Fractal/Lesson_6_Fractal/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_6_Fractal
+{
+    public partial class Form1 : Form
+    {
+        Point[] hexagon = new Point[8];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n;
+
+                Point centre;
+
+                centre = new Point();
+
+                centre.X = 300;
+                centre.Y = 300;
+
+                n = Convert.ToInt32(txtIterations.Text);
+
+                Pen first = new Pen(Color.FromArgb(113, 178, 128), 1);
+                Pen second = new Pen(Color.FromArgb(89, 153, 119), 1);
+                Pen third = new Pen(Color.FromArgb(66, 128, 111), 1);
+                Pen fourth = new Pen(Color.FromArgb(42, 103, 102), 1);
+                Pen fifth = new Pen(Color.FromArgb(19, 78, 94), 1);
+                Pen sixth = new Pen(Color.FromArgb(184, 216, 191), 1);
+
+
+                Graphics g = Graphics.FromHwnd(pbFractal.Handle);
+                g.Clear(Color.Black);
+                Generate_CentreHexagon(n, g, 170, centre, fifth, 2.3);
+                Generate_CentreHexagon(n, g, 140, centre, fourth, 2.3);
+                Generate_CentreHexagon(n, g, 115, centre, third, 2.6);
+                Generate_CentreHexagon(n, g, 80, centre, second, 2.65);
+                Generate_CentreHexagon(n, g, 57, centre, first, 3);
+                Generate_CentreHexagon(n, g, 40, centre, sixth, 3.5);
+            }
+            catch
+            {
+                MessageBox.Show("ERROR: Invalid input.");
+            }
+
+        }
+
+        public void Generate_CentreHexagon(int n, Graphics g, int radius, Point origin, Pen myHexagon, double factor)
+        {
+            if (n == 0)
+                return;
+
+            else
+            {
+                Point p1, p2, p3, p4, p5, p6, p7, p8;
+
+                p1 = new Point();
+                p2 = new Point();
+                p3 = new Point();
+                p4 = new Point();
+                p5 = new Point();
+                p6 = new Point();
+                p7 = new Point();
+                p8 = new Point();
+
+                p1.X = origin.X - radius;
+                p1.Y = origin.Y;
+
+                p2.X = origin.X - Convert.ToInt32(Math.Sqrt((radius * radius) / 2));
+                p2.Y = origin.Y - Convert.ToInt32(Math.Sqrt((radius * radius) / 2));
+
+                p3.X = origin.X;
+                p3.Y = origin.Y - radius;
+
+                p4.X = origin.X + Convert.ToInt32(Math.Sqrt((radius * radius) / 2));
+                p4.Y = origin.Y - Convert.ToInt32(Math.Sqrt((radius * radius) / 2));
+
+                p5.X = origin.X + radius;
+                p5.Y = origin.Y;
+
+                p6.X = origin.X + Convert.ToInt32(Math.Sqrt((radius * radius) / 2));
+                p6.Y = origin.Y + Convert.ToInt32(Math.Sqrt((radius * radius) / 2));
+
+                p7.X = origin.X;
+                p7.Y = origin.Y + radius;
+
+                p8.X = origin.X - Convert.ToInt32(Math.Sqrt((radius * radius) / 2));
+                p8.Y = origin.Y + Convert.ToInt32(Math.Sqrt((radius * radius / 2)));
+
+                Point[] hexagon = new Point[8];
+
+                hexagon[0] = p1;
+                hexagon[1] = p2;
+                hexagon[2] = p3;
+                hexagon[3] = p4;
+                hexagon[4] = p5;
+                hexagon[5] = p6;
+                hexagon[6] = p7;
+                hexagon[7] = p8;
+
+                g.DrawPolygon(myHexagon, hexagon);
+
+                radius = Convert.ToInt32(radius / factor);
+
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[0], myHexagon, factor);
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[1], myHexagon, factor);
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[2], myHexagon, factor);
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[3], myHexagon, factor);
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[4], myHexagon, factor);
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[5], myHexagon, factor);
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[6], myHexagon, factor);
+                Generate_CentreHexagon(n - 1, g, radius, hexagon[7], myHexagon, factor);
+            }
+        }
+       
+    }
+}
